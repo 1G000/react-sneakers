4c127a2f993f5e1726d29dc0946e66b4b71386f0
1G000 <gorbatovskiy.igor@gmail.com>
2022-11-16 06:20:37 +0300

    day2 add likes on cards

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="wrapper clear">
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" />
+          <img width={40} height={40} src="/img/logo.png" alt="logo" />
           <div>
             <h3 className="text-uppercase"> React Sneakers</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -13,18 +13,28 @@ function App() {
         </div>
         <ul className="d-flex">
           <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" />
+            <img width={18} height={18} src="/img/cart.svg" alt="cart" />
             <span>1205 руб.</span>
           </li>
           <li>
-            <img width={18} height={18} src="/img/user.svg" />
+            <img width={18} height={18} src="/img/user.svg" alt="user" />
           </li>
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="mb-40 d-flex align-center justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
         <div className="sneakers d-flex">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
             <img
               width={133}
               height={112}
